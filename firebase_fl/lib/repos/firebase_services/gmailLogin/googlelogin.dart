0fd3fa9a2a414b2203eecfccf74005653c4c9746
Syed Ameenuddin <ameen.syed124@gmail.com>
2024-02-07 08:08:00 +0530

    Update googlelogin.dart
    inapp authenticate

--- firebase_fl/lib/repos/firebase_services/gmailLogin/googlelogin.dart
+++ firebase_fl/lib/repos/firebase_services/gmailLogin/googlelogin.dart
@@ -2,9 +2,19 @@ import 'package:firebase_fl/features/feathersImport.dart';
 
 class firebaseAuthgooogle {
   Future signInWithGoogle() async {
-    GoogleAuthProvider _google = GoogleAuthProvider();
-    FirebaseAuth _auth = await FirebaseAuth.instance;
-    UserCredential sign_auth = await _auth.signInWithProvider(_google);
-    return sign_auth;
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+      final credentail = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+      FirebaseAuth _auth = FirebaseAuth.instance;
+      UserCredential sign_auth = await _auth.signInWithCredential(credentail);
+      return sign_auth;
+    // GoogleAuthProvider _google = GoogleAuthProvider();
+    // FirebaseAuth _auth = await FirebaseAuth.instance;
+    // UserCredential sign_auth = await _auth.signInWithProvider(_google);
+    // return sign_auth;
   }
 }
